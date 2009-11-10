40a1e0f86095f232d2ed1f324c57040fa3d646c0
smartgk <smartgk@gmail.com>
2009-11-10 16:47:08 +0530

    Changed the file structure and removed unnecessary files.

---
+++ GoogleAppEngine/smart-gk/conf/settings.py
@@ -0,0 +1,14 @@
+USE_I18N = True
+
+# Valid languages
+LANGUAGES = (
+    # 'en', 'zh_TW' should match the directories in conf/locale/*
+    ('en', _('English')),
+    ('zh_TW', _('Chinese')),
+    ('hi', _('Hindi')),
+    ('es', _('Spanish')),
+    ('ta', _('Tamil')),
+    )
+
+# This is a default language
+LANGUAGE_CODE = 'en'
--- lastweet/src/config-sample.py
+++
@@ -1,4 +0,0 @@
-# Please input a Twitter account, not necessary, but helpful to provent reaching rate limit
-# And MUST save this as config.py, LasTweet will complains if no config.py
-twitter_username = ''
-twitter_password = ''
